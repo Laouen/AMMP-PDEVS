a7078cb964f6989d1b27aedcb4a976e5f2cd3f20
laouen <lao.facu@gmail.com>
2015-04-06 16:08:21 -0400

    cleaning testing_filter.cpp from comments

--- testing/testing-filters.cpp
+++ testing/testing-filters.cpp
@@ -38,98 +38,6 @@ typedef vector< pair< shared_ptr< model<Time> >, shared_ptr< model<Time> > > > v
 /******** End type definations *********/
 /***************************************/
 
-
-//int main(int argc, char* argv[]) {
-//
-//
-//  /**************************************************************************************************************/
-//  /************************************* Parsing the SBML file **************************************************/
-//  /**************************************************************************************************************/
-//
-//  TiXmlDocument doc;
-//
-//  TiXmlElement * root, * model, * listOfReactions;
-//  map<string, TiXmlElement *> model_lists;
-//
-//  if (argc <= 1){
-//      cout << "one argument must to be defined" << endl;
-//      exit(1);
-//  }
-//
-//  if ( !doc.LoadFile(argv[1]) ){
-//      cout << "fail loading" << endl;
-//      exit(1);
-//  }
-//
-//  root  = doc.FirstChildElement();
-//  model = root->FirstChildElement();
-//
-//  for(TiXmlElement * it = model->FirstChildElement(); it != NULL; it = it->NextSiblingElement()){
-//    model_lists[it->Value()] = it;
-//  }
-//
-//  /**************************************************************************************************************/
-//  /********************************* Creating unit definitions **************************************************/
-//  /**************************************************************************************************************/
-//
-//  // declaring variables
-//  string kind, factors_in_string;
-//  double exponent, multiplier;
-//  int scale;
-//  list<Unit> list_of_units;
-//  list<UnitDefinition> list_of_units_definitions;
-//
-//  for (TiXmlElement * current_unit_definition = model_lists["listOfUnitDefinitions"]->FirstChildElement(); current_unit_definition != NULL; current_unit_definition = current_unit_definition->NextSiblingElement()){
-//    for(TiXmlElement * current_list_of_units = current_unit_definition->FirstChildElement(); current_list_of_units != NULL; current_list_of_units = current_list_of_units->NextSiblingElement()){
-//
-//      list_of_units.clear();
-//      for(TiXmlElement * current_unit = current_list_of_units->FirstChildElement(); current_unit != NULL; current_unit = current_unit->NextSiblingElement()){
-//        kind = current_unit->Attribute("kind");
-//
-//        if (current_unit->Attribute("multiplier") != NULL) multiplier = stod(current_unit->Attribute("multiplier"));
-//        else multiplier = 1;
-//
-//        if (current_unit->Attribute("scale") != NULL) scale = stod(current_unit->Attribute("scale"));
-//        else scale = 0;
-//
-//        if (current_unit->Attribute("exponent") != NULL) exponent = stod(current_unit->Attribute("exponent"));
-//        else exponent = 1;
-//
-//        list_of_units.push_back(Unit(kind, exponent, scale, multiplier));
-//      }
-//
-//      list_of_units_definitions.push_back(UnitDefinition(list_of_units, current_unit_definition->Attribute("id")));
-//    }
-//  }
-//
-//  /**************************************************************************************************************/
-//  /*********************************** End creating unit definitions ********************************************/
-//  /**************************************************************************************************************/
-//
-//  /**************************************************************************************************************/
-//  /*********************************** End parsing the SBML file ************************************************/
-//  /**************************************************************************************************************/
-//
-//
-//
-//  /**************************************************************************************************************/
-//  /************************************** Making the models *****************************************************/
-//  /**************************************************************************************************************/
-//
-//
-//
-//  /**************************************************************************************************************/
-//  /************************************ End making the models ***************************************************/
-//  /**************************************************************************************************************/
-//
-//  return 0;
-//}
-
-
-/**************************************************************************************************************/
-/***************************************** Testing models *****************************************************/
-/**************************************************************************************************************/
-
 int main () {
 
   /**************************************************************************************************************/
@@ -137,7 +45,7 @@ int main () {
   /**************************************************************************************************************/
 
   /*********************************************** TEST 1 *******************************************************/
-  /*
+  
   cout << "Creating the atomic models for the filters:" << endl;
   cout << "enzyme set: Membrane -> cytoplasm: Cytoplasm -> compartment: Cytoplasm space" << endl;
   auto membrane_filter = make_atomic_ptr< filter<Time>, string, string >("enzyme set", "Membrane");
