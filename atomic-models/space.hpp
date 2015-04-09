b5e1484fcda22138a5ae72d7dd5ce27c5d304232
laouen <lao.facu@gmail.com>
2015-04-08 20:32:19 -0400

    working on space atomic model, reesarching about std random library.

--- atomic-models/space.hpp
+++ atomic-models/space.hpp
@@ -4,7 +4,9 @@
 #include <string>
 #include <utility>
 #include <map>
-#include "../data-structures/types.hpp"
+#include <math.h>
+#include <random>
+#include "../data-structures/types.hpp" // number e, metabolite_info_t, enzyme_info_t, SState, Integer
 
 
 using namespace boost::simulation::pdevs;
@@ -14,25 +16,63 @@ using namespace std;
 *********type definations*************
 *************************************/
 
-/******** Basic types ************/
-typedef pair<string, double> molecule;
-
-/******** vector types ************/
-typedef vector<molecule> vectorOfMolecules;
-
 template<class TIME, class MSG>
 class space : public atomic<TIME, MSG>
 {
 private:
-  TIME _next_internal;
-  TIME _interval_time;
+  TIME                              _next_internal;
+  TIME                              _interval_time;
+  map<string, metabolite_info_t>    _metabolites;
+  map<string, enzyme_info_t>        _enzymes;
+  double                            _volume;
+  SState                            _s;
+  // used for
+  random_device                     _rd;
+  mt19937                           _generator;
+  uniform_real_distribution<double> _distribution;
 
 public:
 
-  explicit space(const vector<reaction_input> reactions) noexcept {
-  }
+  explicit space(
+    TIME                            other_next_internal,
+    TIME                            other_interval_time,
+    map<string, metabolite_info_t>  other_metabolites
+    map<string, enzyme_info_t>      other_enzymes,
+    double                          other_volume,
+    ) noexcept :
+  _next_internal(other_next_internal),
+  _interval_time(other_interval_time),
+  _metabolites(other_metabolites),
+  _enzymes(other_enzymes),
+  _volume(other_volume),
+  _s(SState::SELECTING),
+  _rd(),
+  _generator(_rd),
+  _distribution(0.0,1.0) {}
 
-  void internal() noexcept { 
+  void internal() noexcept {
+
+    if (_s == SState::SELECTING) {
+      
+      for (map<string, metabolite_info_t>::iterator it = _metabolites.begin(); it != _metabolites.end(); ++it) {
+        it->to_send = this->weightedRandomBool(it->amount);
+      }
+
+      _next_internal  = TIME(0);
+      _s              = SState::SENDING;
+    } else if (_s == SState::SENDING) {
+
+      for (map<string, metabolite_info_t>::iterator it = _metabolites.begin(); it != _metabolites.end(); ++it) {
+
+        if (it->to_send) {
+          it->amount  = 0;
+          it->to_send = false;
+        }
+      }
+
+      _next_internal  = _interval_time;
+      _s              = SState::SELECTING;
+    }
   }
 
   TIME advance() const noexcept {
@@ -41,10 +81,41 @@ public:
   }
 
   vector<MSG> out() const noexcept {
+
+    vector<MSG> result                    = {};
+    vector<Integer> distributed_reactants = {};
+    MSG current_message;
+
+    if (_s == SState::SENDING) {
+      for (map<string, metabolite_info_t>::const_iterator it = _metabolites.cbegin(); it != _metabolites.cend(); ++it) {
+        
+        if (it->second.to_send) {
+          distributed_reactants.clear();
+          distributed_reactants.resize(it->second.enzymes.size())
+          randomDistribution(distributed_reactants, it->second.amount);
+          current_message.specie = it->first; 
+
+          for (int i = 0; i < distributed_reactants.size(); ++it) {
+
+            current_message.amount  = distributed_reactants[i];
+            current_message.to      = it->second.enzymes[i];
+            result.push_back(current_message);
+          }
+        } 
+      }
+    }
+
+    return result;
   }
 
-  void external(const std::vector<MSG>& mb, const TIME& t) noexcept {
-    
+  void external(const vector<MSG>& mb, const TIME& t) noexcept {
+
+    for (vector<MSG>::const_iterator it = mb.cbegin(); it != mb.cend(); ++it) {
+      
+      this->addToMetabolites(it->specie, it->amount);
+    }
+
+    _next_internal = _interval_time - t;
   }
 
   virtual void confluence(const std::vector<MSG>& mb, const TIME& t) noexcept {
@@ -52,6 +123,72 @@ public:
     internal();
     external(mb, TIME(0));
   }
+
+  /***************************************
+  ********* helper functions *************
+  ***************************************/
+
+  void addToMetabolites(string n, Integer a){
+    metabolite_info_t new_metabolite;
+
+    if (a > 0) {
+      if (_metabolites.find(n) != _metabolites.end()) {
+
+        _metabolites.at(n).amount += a;
+      } else {
+        
+        new_metabolite.amount   = a;
+        new_metabolite.to_send  = false;
+        new_metabolite.enzymes  = lookForEnzymes(n);
+        _metabolites[n]         = new_metabolite;
+      }
+    }
+  }
+
+  vector<Adress> lookForEnzymes(string n) const {
+    vector<Adress> result;
+
+    for (map<string, enzyme_info_t>::const_iterator it = _enzymes.cbegin(); it != _enzymes.cend(); ++it) {
+      if (belong(n, it->second.reactants)) {
+        result.push_back(it->second.location);
+      }
+    }
+
+    return result;
+  }
+
+  bool belong(string n, const vector<string>& ls) const {
+    bool result = false;
+
+    for (vector<string>::const_iterator it = ls.cbegin(); it != ls.cend(); ++it) {
+      if (n == *it) {
+        result true;
+        break;
+      }
+    }
+    return result;
+  }
+
+  bool weightedRandomBool(Integer a) const {
+
+    double proportion = _volume / (double)a;
+    double threshold  = (double)1.0 / pow( (double)e, (double)_factor*proportion )
+    
+    return _distribution(_generator) < threshold ;
+  }
+
+  void randomDistribution(vector<Integer>& ds, Integer a) {
+    Integer current_amount;
+
+    for (int i = 0; i < ds.size(); ++i) {
+      ds[i] = 0;
+    }
+
+    for (Integer i = 0; i < a; ++i) {
+      ds[rand() % ds.size()] += 1;
+    }
+  }
 };
+  
 
 #endif // BOOST_SIMULATION_PDEVS_CONTROLER_H
--- data-structures/types.hpp
+++ data-structures/types.hpp
@@ -1,17 +1,19 @@
 #ifndef BOOST_SIMULATION_TYPES_H
 #define BOOST_SIMULATION_TYPES_H
 
-
+#include <iostream>
+#include <string>
 #include <list>
 #include <vector>
 #include <map>
 
-/******************************************/
-/********** Type definations **************/
-/******************************************/
+#define e 2.71828182845904523536028747135266249775724709369995
 
 using namespace std;
 
+/******************************************/
+/********** Enums and renames *************/
+/******************************************/
 
 enum class RState { REJECTING, REACTING, SELECTING };
 enum class SState { SENDING, SELECTING };
@@ -19,6 +21,12 @@ enum class Way { RTP, PTR };
 
 using Integer = unsigned long long;
 using SetOfMolecules  = map<string, Integer>;
+
+
+
+/******************************************/
+/******** End enums and renames ***********/
+/******************************************/
 
 template<class TIME>
 struct Task {
@@ -74,6 +82,169 @@ struct Task {
 
 template<class TIME>
 using TaskQueue = list< Task<TIME> >;
+
+
+/*******************************************/
+/**************** Message ******************/
+/*******************************************/
+
+struct Adress {
+
+  string _organelle;
+  string _cytoplasm;
+  string _ensyme_set;
+  string _compartment;
+  string _enzyme;
+
+  Adress(string other_organelle, string other_cytoplasm, string other_ensyme_set, string other_compartment, string other_enzyme)
+  : _organelle(other_organelle), _cytoplasm(other_cytoplasm), _ensyme_set(other_ensyme_set), _compartment(other_compartment), _enzyme(other_enzyme) {}
+
+  Adress()
+  : _organelle(""), _cytoplasm(""), _ensyme_set(""), _compartment(""), _enzyme("") {}
+
+  Adress(const Adress& other)
+  : _organelle(other._organelle), _cytoplasm(other._cytoplasm), _ensyme_set(other._ensyme_set), _compartment(other._compartment), _enzyme(other._enzyme) {}
+  
+  Adress(Adress* other)
+  : _organelle(other->_organelle), _cytoplasm(other->_cytoplasm), _ensyme_set(other->_ensyme_set), _compartment(other->_compartment), _enzyme(other->_enzyme) {}
+
+
+
+  string atModel(string model_type) const{
+    
+    string result = "";
+    if(model_type == "organelle") {
+      
+      result = _organelle;
+    } else if(model_type == "cytoplasm") {
+      
+      result = _cytoplasm;
+    } else if(model_type == "enzyme set") {
+      
+      result = _ensyme_set;
+    } else if(model_type == "compartment") {
+      
+      result = _compartment;
+    } else if(model_type == "enzyme") {
+      
+      result = _enzyme;
+    }
+
+    return result;
+  }
+
+  void setModel(string model_type, string new_value) {
+
+    if(model_type == "organelle") {
+      
+      _organelle = new_value;
+    } else if(model_type == "cytoplasm") {
+      
+      _cytoplasm = new_value;
+    } else if(model_type == "enzyme set") {
+      
+      _ensyme_set = new_value;
+    } else if(model_type == "compartment") {
+      
+      _compartment = new_value;
+    } else if(model_type == "enzyme") {
+      
+      _enzyme = new_value;
+    }
+  }
+
+  void clear() {
+    _organelle    = "";
+    _cytoplasm    = "";
+    _ensyme_set   = "";
+    _compartment  = "";
+    _enzyme     = "";
+  }
+
+};
+
+ostream& operator<<(ostream& os, Adress to) {
+  os << "Organelle: " << to._organelle << endl;
+  os << "Cytoplasm: " << to._cytoplasm << endl;
+  os << "Enzyme set: " << to._ensyme_set << endl;
+  os << "Compartment: " << to._compartment << endl;
+  os << "Enzyme: " << to._enzyme << endl;
+  return os;
+}
+
+struct Message {
+  
+  Adress to;
+  string specie;
+  Integer amount;
+
+  Message(Adress other_to, string other_specie, Integer other_amount)
+  : to(other_to), specie(other_specie), amount(other_amount) {}
+
+  Message()
+  :to(), specie(""), amount(0) {}
+
+  Message(const Message& other)
+  : to(other.to), specie(other.specie), amount(other.amount) {}
+
+  Message(Message* other)
+  : to(other->to), specie(other->specie), amount(other->amount) {}
+
+  bool sendTo(string model_type, string new_value) {
+    to.setModel(model_type, new_value);
+
+    return (to.atModel(model_type) == new_value);
+  }
+
+  void clear() {
+    to.clear();
+    specie = "";
+    amount = 0;
+  }
+};
+
+ostream& operator<<(ostream& os, Message msg) {
+  os << "To: " << endl << msg.to << endl;
+  os << "Specie: " << msg.specie << endl;
+  os << "Amount: " << msg.amount << endl;
+  return os;
+}
+
+/*******************************************/
+/************** End Message ****************/
+/*******************************************/
+
+
+struct metabolite_info_t {
+  
+  Integer         amount;
+  bool            to_send;
+  vector<Adress>  enzymes;
+
+  metabolite_info_t()
+  : amount(0), to_send(false), enzymes() {}
+
+  metabolite_info_t(string other_specie, Integer other_amount, bool other_to_send, vector<Adress> other_enzymes)
+  : amount(other_amount), to_send(other_to_send), enzymes(other_enzymes) {}
+
+  metabolite_info_t(const metabolite_info_t& other)
+  : amount(other.amount), to_send(other.to_send), enzymes(other.enzymes) {}
+};
+
+struct enzyme_info_t {
+
+  Adress          location;
+  vector<string>  reactants;
+
+  enzyme_info_t()
+  : location(), reactants() {}
+
+  enzyme_info_t(Adress other_location, vector<string> other_reactants)
+  : location(other_location), reactants(other_reactants) {}
+
+  enzyme_info_t(const enzyme_info_t& other)
+  : location(other.location), reactants(other.reactants) {}
+};
 
 
 /******************************************/
