fa5f285a63338db87be84baf2106890f99ed5d91
Laouen <laouen.belloli@gmail.com>
2017-12-02 18:38:27 -0300

    modified tests sbml  model generator to use correct DEFINE_integer in gflags

--- tests/utilities/generate_sbml.py
+++ tests/utilities/generate_sbml.py
@@ -9,13 +9,9 @@ from tqdm import tqdm
 
 def main(FLAGS):
 
-    models_range = len(FLAGS.reaction_amount)
-    assert models_range == len(FLAGS.species_amount) and models_range == len(FLAGS.max_stoichimetry_elements)
+    for i in tqdm(range(len(FLAGS.reaction_amount))):
 
-    for i in tqdm(range(models_range)):
         reaction_amount = int(FLAGS.reaction_amount[i])
-        species_amount = int(FLAGS.species_amount[i])
-        max_stoichimetry_elements = int(FLAGS.max_stoichimetry_elements[i])
 
         reaction_amounts = {
             ('c', 'bulk'): reaction_amount,
@@ -27,13 +23,13 @@ def main(FLAGS):
         }
 
         compartments = ['c', 'e', 'p']
-        species_amounts = {cid: species_amount for cid in compartments}
+        species_amounts = {cid: FLAGS.species_amount for cid in compartments}
 
         model_id = '_'.join([
             FLAGS.model_name,
-            str(reaction_amount),
-            str(species_amount),
-            str(max_stoichimetry_elements)
+            str(FLAGS.reaction_amount[i]),
+            str(FLAGS.species_amount),
+            str(FLAGS.max_stoichimetry_elements)
         ])
 
         generator = SBMLTestGenerator(model_id + '.xml',
@@ -41,7 +37,7 @@ def main(FLAGS):
                                       compartments,
                                       reaction_amounts,
                                       species_amounts,
-                                      max_stoichimetry_elements)
+                                      FLAGS.max_stoichimetry_elements)
         generator.create_listOfCompartments()
         generator.create_listOfSpecies()
         generator.create_listOfReactions()
@@ -52,10 +48,9 @@ if __name__ == '__main__':
 
     gflags.DEFINE_list('reaction_amount', [10], 'A list with the amounts of reactions in each compartment',
                        short_name='r')
-    gflags.DEFINE_list('species_amount', [100], 'A list with the amounts of species in each compartment',
-                       short_name='s')
-    gflags.DEFINE_list('max_stoichimetry_elements', [10], 'The maximum amount of Product and '
-                       'Reactant metabolites a reaction can have', short_name='m')
+    gflags.DEFINE_integer('species_amount', 100, 'The amounts of species in each compartment', short_name='s')
+    gflags.DEFINE_integer('max_stoichimetry_elements', 10, 'The maximum amount of Product and '
+                          'Reactant metabolites a reaction can have', short_name='m')
     gflags.DEFINE_string('model_name', None, 'The SBML model name', short_name='n')
 
     gflags.MarkFlagAsRequired('model_name')
