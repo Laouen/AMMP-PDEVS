8f5532e73e6eca47fc793587b8b4bec66850e900
Laouen <laouen.belloli@gmail.com>
2017-12-03 12:16:43 -0300

    Removing from git the model-specification. this folder is now in Dropbox

--- model-specification/svg2png-crop.sh
+++
@@ -1,9 +0,0 @@
-# Tuned version of matemarote/utils/svg2png.sh to crop svgs from excesive
-# transparency
-if [ -z "$1" ]; then
-    files="*.svg"
-else
-    files=$1
-fi
-{ for x in *.svg; do inkscape -D --export-png="$x.png" "$x"; done }
-rename -f 's/(.*).svg.png/$1.png/' *.svg.png
